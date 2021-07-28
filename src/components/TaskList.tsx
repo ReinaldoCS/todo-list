7faae817da8a381a73f5888ce56545c837a8bafe
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2021-07-28 17:31:57 -0300

    Marcando task como completa

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -33,6 +33,13 @@ export function TaskList() {
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
+    const taskIndex = tasks.findIndex((task => task.id === id));
+    const newTasks = [...tasks];
+  
+    newTasks[taskIndex].isComplete =  !newTasks[taskIndex].isComplete;
+  
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
